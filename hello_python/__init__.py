ba478fedff48df1ac0cafd4b8969746567d555b3
madfishOD <madfish.od@gmail.com>
2025-10-04 21:49:08 +0300

    init restored

--- hello_python/__init__.py
+++ hello_python/__init__.py
@@ -2,42 +2,7 @@
 from .operators import PrintMe
 from .panel import HelloPanel
 
-import fiftyone.operators as foo
-import fiftyone.operators.types as types
-
-class HelloPanel(foo.Panel):
-    @property
-    def config(self):
-        return foo.PanelConfig(
-            name="@madfish/hello_python.HelloPanel",
-            label="Hello Panel (Python)",
-            icon="smile",            # optional icon
-            allow_multiple=False,    # only one instance
-        )
-
-    def on_load(self, ctx):
-        # optional: initialize panel state
-        ctx.panel.set_state("message", "Hello World!")
-
-    def render(self, ctx):
-        # retrieve current state
-        message = ctx.panel.state.get("message", "Hello World!")
-
-        # operator trigger
-        op = ctx.request_op("@madfish/hello_python/print_me")
-
-        # build UI
-        obj = types.Object()
-        obj.str("label", label=message)
-        obj.button(
-            "click_me",
-            label="Click Me!",
-            on_click=op,
-        )
-
-        # return as a Property (important!)
-        return types.Property(obj, view=types.View(label="Hello Panel"))
-
 def register(plugin):
+    """Registers the plugins in this module."""
     plugin.register(PrintMe)
     plugin.register(HelloPanel)
